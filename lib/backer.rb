dce99f85816736d5399ffbbccf2d3b734f5014a5
Mitchel Parish <mitchelparish@gmail.com>
2019-01-31 01:29:42 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,14 @@
 class Backer
   attr_accessor :backed_projects
-  attr_reader :name 
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project 
   end
 
 
